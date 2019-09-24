c5e1abb0763db6849e9b121412f895972ac07da1
Norm Schaeffer <nschaeffer@enkadia.com>
2019-09-24 15:42:41 -0500

    code cleanup

--- DisplayTest/MainPage.xaml.cs
+++ DisplayTest/MainPage.xaml.cs
@@ -1,30 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Enkadia.Synexsis.Components.Displays.Samsung;
+using Enkadia.Synexsis.ComponentFramework.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace DisplayTest
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
+
+        private ExLink Display;
+        private IServiceCollection serviceCollection;
+        private IServiceProvider sp;
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            Device();
+        }
+
+        public void Device()
+        {
+            serviceCollection = new ServiceCollection();
+            serviceCollection.AddSynexsis();
+            serviceCollection.AddTransient<ExLink>();
+            sp = serviceCollection.BuildServiceProvider();
+            Display = sp.GetService<ExLink>();
+        }
+
+        public async void BtnPowerOff_OnClick(object sender, RoutedEventArgs e)
+        {
+            await Display.PowerOff();
+        }
+
+        public async void BtnPowerOn_OnClick(object sender, RoutedEventArgs e)
+        {
+            await Display.PowerOn();
         }
     }
 }
