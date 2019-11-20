bf8fc3d8775b8643be19d1cd6c601e548f04e156
Norm Schaeffer <nschaeffer@enkadia.com>
2019-11-20 12:44:19 -0600

    Added Sharp displays

--- DisplayTest/MainPage.xaml.cs
+++ DisplayTest/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Enkadia.Synexsis.Components.Displays.Samsung;
+using Enkadia.Synexsis.Components.Displays.Sharp;
 using Enkadia.Synexsis.ComponentFramework.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +13,9 @@ namespace DisplayTest
     public sealed partial class MainPage : Page
     {
 
-        private ExLink Display;
+        private ExLink FrontDisplay;
+        private SharpDisplay Rear1;
+        private SharpDisplay Rear2;
         private IServiceCollection serviceCollection;
         private IServiceProvider sp;
 
@@ -28,18 +31,25 @@ namespace DisplayTest
             serviceCollection = new ServiceCollection();
             serviceCollection.AddSynexsis();
             serviceCollection.AddTransient<ExLink>();
+            serviceCollection.AddTransient<SharpDisplay>();
             sp = serviceCollection.BuildServiceProvider();
-            Display = sp.GetService<ExLink>();
+            FrontDisplay = sp.ResolveWith<ExLink>("FrontDisplay");
+            Rear1 = sp.ResolveWith<SharpDisplay>("Rear1");
+            Rear2 = sp.ResolveWith<SharpDisplay>("Rear2");
         }
 
-        public async void BtnPowerOff_OnClick(object sender, RoutedEventArgs e)
+        public void BtnPowerOff_OnClick(object sender, RoutedEventArgs e)
         {
-            await Display.PowerOff();
+            FrontDisplay.PowerOff();
+            Rear1.PowerOff();
+            Rear2.PowerOff();
         }
 
-        public async void BtnPowerOn_OnClick(object sender, RoutedEventArgs e)
+        public void BtnPowerOn_OnClick(object sender, RoutedEventArgs e)
         {
-            await Display.PowerOn();
+            FrontDisplay.PowerOn();
+            Rear1.PowerOn();
+            Rear2.PowerOn();
         }
     }
 }
